f3d0b30f866dfabec507844af0e7883e341bc068
Anastasiu Tudor <anastasiu.tudor@gmail.com>
2022-01-18 17:31:43 +0200

    http

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,2 +1,2 @@
-const baseURL = "https://api.weatherstack.com/";
+const baseURL = "http://api.weatherstack.com/";
 export const currentURL = `${baseURL}/current?access_key=${process.env.REACT_APP_API_KEY}&query=`
